644da17ce0a0b9528e1990706364d533b52fa2aa
fralonra <fralonra@aliyun.com>
2019-12-19 19:44:14 +0800

    Update(). add doc and LICENSE

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ebiten-scene
 
+[![GoDoc](https://godoc.org/github.com/fralonra/ebiten-scene?status.svg)](https://godoc.org/github.com/fralonra/ebiten-scene)
+![License](https://img.shields.io/badge/License-MIT-blue.svg)
+[![Go Report Card](https://goreportcard.com/badge/github.com/fralonra/ebiten-scene)](https://goreportcard.com/report/github.com/fralonra/ebiten-scene)
+
 A simple scene manager for [ebiten](https://github.com/hajimehoshi/ebiten).
--- scene.go
+++ scene.go
@@ -9,14 +9,18 @@ var (
 	scenes       = make(map[GameSceneKey]*GameScene)
 )
 
+// Scene returns the current scene.
 func Scene() GameScene {
 	return currentScene
 }
 
+// Set registers a GameScene with the given key.
 func Set(key GameSceneKey, scene GameScene) {
 	scenes[key] = &scene
 }
 
+// SwitchWith changes the current scene to the scene with the given key.
+// It also recieves a function which allows you to access the new scene.
 func SwitchWith(key GameSceneKey, withFunc func(GameScene)) {
 	scene := scenes[key]
 	if scene == nil {
@@ -34,10 +38,35 @@ func SwitchWith(key GameSceneKey, withFunc func(GameScene)) {
 	currentScene.Start()
 }
 
+// Switch changes the current scene to the scene with the given key.
 func Switch(key GameSceneKey) {
 	SwitchWith(key, nil)
 }
 
+// Update is the function called every frame by ebiten.
+func Update(screen *ebiten.Image) error {
+	if currentScene == nil {
+		return nil
+	}
+	currentScene.Update()
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	currentScene.Draw(screen)
+	return nil
+}
+
+// GameScene is the interface that all the game scene should implement.
+//
+// Draw is the function where rendering takes place.
+//
+// Update is the function where game logic updates. It is invoked before Draw.
+//
+// Start is the function called every time you switch to the scene.
+//
+// Stop is the function called every time you exit the scene.
 type GameScene interface {
 	Draw(screen *ebiten.Image)
 	Update()
@@ -45,4 +74,5 @@ type GameScene interface {
 	Stop()
 }
 
+// GameSceneKey represents the identifier of each scene.
 type GameSceneKey int
